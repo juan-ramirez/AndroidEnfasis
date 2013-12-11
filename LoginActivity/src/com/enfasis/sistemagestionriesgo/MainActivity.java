9e451e5d78afc55712dc113531b102249a4c40a2
mixergtz <mixergutierrez@gmail.com>
2013-12-10 20:16:46 -0500

    Cambios varios: Menu con cambio de color en el hover, intents se hacen detectando los elementos. Hay un error y es que no detecta la tabla "riesgos"

--- LoginActivity/src/com/enfasis/sistemagestionriesgo/MainActivity.java
+++ LoginActivity/src/com/enfasis/sistemagestionriesgo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.enfasis.sistemagestionriesgo;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import android.app.Activity;
@@ -8,10 +9,13 @@ import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -19,6 +23,7 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 
 	SQLiteDatabase database;
+    private static String DB_NAME = "db_riesgos.s3db";
 
 	Intent generalIntent;
 
@@ -27,16 +32,49 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_main);
+		inicializarObjetos();
+		inicializarDatabase();
 		crearMenu();
 
 	}
+	
+	private void inicializarDatabase(){
+		DataBaseHelper dbHelper = new DataBaseHelper(this);
 
+		try {
+
+			dbHelper.createDataBase();
+
+		} catch (IOException ioe) {
+
+			throw new Error("Unable to create database");
+
+		}
+
+		try {
+
+			dbHelper.openDataBase();
+
+		} catch (SQLException sqle) {
+
+			throw sqle;
+
+		}
+		
+		database = dbHelper.myDataBase;
+
+	}
+
+	private void inicializarObjetos(){
+       database = openOrCreateDatabase(DB_NAME, SQLiteDatabase.OPEN_READWRITE, null);
+	}
+	
 	private void crearMenu() {
 		String[] opcion1 = { "Crear", "Creacion de formatos", "crear_mini" };
-		String[] opcion2 = { "Crear", "Creacion de formatos", "crear_mini" };
-		String[] opcion3 = { "Crear", "Creacion de formatos", "crear_mini" };
-		String[] opcion4 = { "Crear", "Creacion de formatos", "crear_mini" };
-		String[] opcion5 = { "Crear", "Creacion de formatos", "crear_mini" };
+		String[] opcion2 = { "Modificar", "Creacion de formatos", "crear_mini" };
+		String[] opcion3 = { "Consultar", "Creacion de formatos", "crear_mini" };
+		String[] opcion4 = { "Control", "Creacion de formatos", "crear_mini" };
+		String[] opcion5 = { "Grafica", "Creacion de formatos", "crear_mini" };
 		ArrayList<String[]> arrayListMenu = new ArrayList<String[]>();
 		arrayListMenu.add(opcion1);
 		arrayListMenu.add(opcion2);
@@ -46,8 +84,41 @@ public class MainActivity extends Activity {
 		ListView listViewMenu = (ListView) findViewById(R.id.listViewMenuPpal);
 		MenuPpalAdapter adapter = new MenuPpalAdapter(this, arrayListMenu);
 		listViewMenu.setAdapter(adapter);
+		
+		listViewMenu.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long id) {
+				// TODO Auto-generated method stub
+				switch (position) {
+				case 0:
+					goCrear();
+					break;
+				case 1:
+					goModificar();
+					break;
+				case 2:
+					goConsultar();
+					break;
+				case 3:
+					goControl();
+					break;
+				case 4:
+					goGrafico();
+					break;
+
+					
+				default:
+					break;
+				}
+				
+			}
+		});
 
 	}
+	
+	
 
 	public void goCrear() {
 		generalIntent = new Intent(this, CrearActivity.class);
